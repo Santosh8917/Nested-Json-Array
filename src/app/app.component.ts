9854ddda43070c61143d27fa5824db161b96989a
Santosh Behera01 <Santosh.Behera01@godigit.com>
2023-01-22 22:55:14 +0530

    Nested json array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nest';
+//createdData:any = [];
+demodata = {
+  "orders":{
+    "total" : 17,
+    "data": [
+      {
+        "id":10896,
+        "createdAt":"2019-12-30T07:18:19.182Z",
+        "amount":6000,
+        "dealer":{
+          "company":{
+            "name":"UjjwalTechnicalTips"
+          }
+        },
+        "transactions":[
+          {},
+          {},
+          {},
+          {
+            "stripe" : {
+              "status":"Succeeded",
+              "card":{
+                "funding":"unknown"
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
+
+
+
+
 }
